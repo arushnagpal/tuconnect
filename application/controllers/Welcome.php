10fa159c198e08d7e1cc49b1c6f368e347415aed
Arush Nagpal <arushngpl16@gmail.com>
2016-02-18 18:52:15 +0530

    Added the homepage template

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -18,6 +18,14 @@ class Welcome extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
+
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->database();
+		$this->load->library('form_validation');
+		$this->load->helper('url');
+	}
 	public function index()
 	{
 		$this->load->view('welcome_message');
